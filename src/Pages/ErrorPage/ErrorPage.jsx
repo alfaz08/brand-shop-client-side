e50fb9c970ad79a4e5cd686dd99f28dcd2a38e28
Alfaz Hossain <alfaz.cse@gmail.com>
2023-10-17 23:07:58 +0600

    errorpage is added

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 const ErrorPage = () => {
   return (
     <div>
-    <img className="mx-auto mt-28" src="/src/assets/error.jpg" alt="" />
-    <Link to="/" className="flex justify-center font-extrabold mt-2"><span>Go Home</span></Link>
+    <img className="mx-auto mt-28" src='https://i.ibb.co/72ZyFpK/404-page-cover.jpg' alt="" />
+    <Link to="/" className="flex justify-center font-extrabold mt-2 text-4xl"><span>Go Home</span></Link>
   </div>
   );
 };
