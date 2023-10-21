cf1e65b60ea3efed41a1e2c3dfdf5d0e6383b12d
Alfaz Hossain <alfaz.cse@gmail.com>
2023-10-21 13:53:55 +0600

    light and dark mode is added

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,19 +1,9 @@
-import { useState } from "react";
+
 import Navbar from "../components/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
 const MainLayout = () => {
 
 
-  // const [darkMode, setDarkMode] = useState(false);
-
-  // const toggleDarkMode = () => {
-  //   setDarkMode(!darkMode);
-  // };
-
-  // // <div className={`App ${darkMode ? 'dark' : ''}`}>
-  // //     <button onClick={toggleDarkMode}>Toggle Dark Mode</button>
-  // //     <p>This is a sample text.</p>
-  // //   </div>
  
 
   return (
